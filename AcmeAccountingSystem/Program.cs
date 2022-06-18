918d8abd42b3326020a164225f49d4e7db328de1
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-06-18 16:07:48 -0700

    Acme Accounting System Check-in

---
+++ AcmeAccountingSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AcmeAccountingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+        }
+    }
+}
