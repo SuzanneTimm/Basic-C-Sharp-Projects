67b505005e0559e0c2c638c92b6098dd39880288
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-06-20 14:09:22 -0700

    Add files via upload
    Boolean Logic Assignment

---
+++ BooleanLogicAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Boolean Logic Assignment");
+            Console.WriteLine(0);
+
+            Console.Write("What is your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Have you ever had a DUI? ");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.Write("How many speeding tickets do you have? ");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool qualifies = age > 15 && !dui && tickets < 3;
+
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(qualifies);
+
+            
+
+        }
+    }
+}
