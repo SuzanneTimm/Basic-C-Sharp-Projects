bef8d19ebc0a031d2c82957a42d87e6c742ae629
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-06-19 18:06:56 -0700

    Add files via upload
    Income Comparison Assignment

---
+++ IncomeComparisonAssignment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace IncomeComparisonAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Assignment");
+            Console.WriteLine();
+
+            Console.WriteLine("Person 1");
+            Console.Write("  Hourly Rate ");
+            decimal person1HourlyRate = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("  Hours Worked Per Week ");
+            decimal person1HoursWorked = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Person 2");
+            Console.Write("  Hourly Rate ");
+            decimal person2HourlyRate = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("  Hours Worked Per Week ");
+            decimal person2HoursWorked = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Annual salary of Person 1");
+            decimal person1AnnualSalary = person1HourlyRate * person1HoursWorked * 52;
+            Console.WriteLine(person1AnnualSalary);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Annual salary of Person 2");
+            decimal person2AnnualSalary = person2HourlyRate * person2HoursWorked * 52;
+            Console.WriteLine(person2AnnualSalary);
+
+            Console.WriteLine();
+            bool person1MakesMoreThanPerson2 = person1AnnualSalary > person2AnnualSalary;
+            Console.WriteLine("Person 1 makes more money than Person 2");
+            Console.WriteLine(person1MakesMoreThanPerson2);
+        }
+    }
+}
