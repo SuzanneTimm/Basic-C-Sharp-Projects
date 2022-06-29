ef4b1ebff7ede6bbf7854951a0d21fba70ade4aa
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-06-29 14:55:41 -0700

    String Assignment

---
+++ StringAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str1 = "I am a huge fan of movies, ";
+            String str2 = "pizza, ";
+            String str3 = "and beer.";
+
+            var str = String.Concat(str1, str2, str3);
+            Console.WriteLine(str);           
+
+            str = str.ToUpper();
+            Console.WriteLine(str);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("This is the first line of text.");
+            sb.AppendLine("This is the second line of text.");
+            sb.AppendLine("This is the third line of text.");
+            sb.AppendLine("This is the fourth line of text.");
+            sb.AppendLine("This is the fifth line of text.");
+            sb.Append(123);
+            sb.AppendLine();
+
+            string result = sb.ToString();
+            Console.WriteLine(result);
+        }
+    }
+}
