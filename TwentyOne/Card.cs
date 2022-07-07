d9a2192b9c7fa5a2696bd77d2daf84b1647197ba
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-07 12:12:37 -0700

    Twenty One (code help)

---
+++ TwentyOne/Card.cs
@@ -0,0 +1,20 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwentyOne
+{
+    class Card
+    {
+        public Card()
+        {
+            Suit = "Spades";
+            Face = "Two";
+        }
+        public string Suit { get; set; }
+        public string Face { get; set; }
+    }
+}
+
+
+ 
