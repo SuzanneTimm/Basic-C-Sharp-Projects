c809ff0d023eb16cf06c0b40dba150ec9053ad13
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-07 15:33:13 -0700

    Method Assignment 1

---
+++ MethodAssignment/MyMath.cs
@@ -0,0 +1,20 @@
+﻿namespace MethodAssignment
+{
+    public class MyMath
+    {
+        public int MultiplyByTen(int num)
+        {
+            return num * 10;
+        }
+
+        public int DivideByTen(int num)
+        {
+            return num / 10;
+        }
+
+        public int Squared(int num)
+        {
+            return num * num;
+        }
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to use for math operations?");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            MyMath math = new MyMath();
+            int numTimes10 = math.MultiplyByTen(num);
+            int numDiv10 = math.DivideByTen(num);
+            int numSquared = math.Squared(num);
+
+            Console.WriteLine(num + " times 10 = " + numTimes10);
+            Console.WriteLine(num + " divided by 10 = " + numDiv10);
+            Console.WriteLine(num + " squared = " + numSquared);
+        }
+    }   
+}
