6ed94ae54d7f07b488698082b4f9f411df82cc55
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-07 19:10:07 -0700

    Classes and Methods Assignment 1

---
+++ ClassesAndMethodsAssignment1/MyMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndMethodsAssignment1
+{
+    public class MyMath
+    {
+        public int AddTen(int num)
+        {
+            return num + 10;
+        }
+
+        public int AddTen(decimal num)
+        {
+            return (int)(num + 10);
+        }
+
+        public int AddTen(string str)
+        {
+            return Convert.ToInt32(str) + 10;
+        }
+    }
+}
---
+++ ClassesAndMethodsAssignment1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassesAndMethodsAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Take a number and add 10.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            MyMath math = new MyMath();            
+            
+            int numPlus10 = math.AddTen(num);
+            Console.WriteLine(numPlus10);
+
+            Console.WriteLine("Enter a decimal number.");
+            decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+            numPlus10 = math.AddTen(num2);
+            Console.WriteLine(numPlus10);
+
+            Console.WriteLine("Enter a string. ");
+            string str = Console.ReadLine();
+
+            numPlus10 = math.AddTen(str);
+            Console.WriteLine(numPlus10);
+
+
+
+
+
+        }
+    }
+}
