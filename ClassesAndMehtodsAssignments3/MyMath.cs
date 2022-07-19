7647dc208132c60a00af50115fb44b69872d0dd2
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-19 15:12:07 -0700

    Classes and Methods Assignment 3

---
+++ ClassesAndMehtodsAssignments3/MyMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndMehtodsAssignments3
+{
+   public class MyMath
+    {
+        public void DoAdd(int num1, int num2)
+        {
+            int result = num1 + 25;
+            Console.WriteLine("num2 = " + num2);
+        }
+    }
+}
---
+++ ClassesAndMehtodsAssignments3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassesAndMehtodsAssignments3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+
+            math.DoAdd(5, 10);
+            math.DoAdd(num2: 5, num1: 10);
+        }
+    }
+}
