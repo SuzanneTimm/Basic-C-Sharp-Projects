d3c7591f376250239a349c64d48e98a5e49ce791
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-19 14:13:33 -0700

    Classes and Methods Assignment 2

---
+++ ClassesAndMethodsAssignment2/MyMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndMethodsAssignment2
+{
+    public class MyMath
+    {
+        public int DoAdd(int num1,int num2 = 10)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ ClassesAndMethodsAssignment2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassesAndMethodsAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+
+            Console.WriteLine("Enter first number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number (this can be blank).");
+            string str2 = Console.ReadLine();
+
+            int answer;
+            if (str2 == "")
+            {
+                answer = math.DoAdd(num1);   
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(str2);
+                answer = math.DoAdd(num1, num2);
+            }
+
+            Console.WriteLine("The answer is " + answer);
+        }
+    }
+}
