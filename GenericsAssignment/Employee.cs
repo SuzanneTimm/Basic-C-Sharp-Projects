3b4f5697b6c537ad77d3098e3371f58be4d73f56
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-19 21:33:31 -0700

    Generics Assignment

---
+++ GenericsAssignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace GenericsAssignment
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+
+        public string Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public static bool operator ==(Employee<T> employee1, Employee<T> employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+
+        public static bool operator != (Employee<T> employee1, Employee<T> employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+    }
+}
+
---
+++ GenericsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> things1 = new Employee<string>();
+            things1.Things = new List<string>() { "books", "cars", "trucks" };
+
+            Employee<int> things2 = new Employee<int>();
+            things2.Things = new List<int>() { 1, 2, 3, 4 };
+
+            foreach (string items1 in things1.Things)
+            {
+                Console.WriteLine(items1);
+            }
+
+            foreach (int items2 in things2.Things)
+            {
+                Console.WriteLine(items2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
