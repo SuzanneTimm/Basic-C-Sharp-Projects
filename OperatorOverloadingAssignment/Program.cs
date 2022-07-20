d111e2ddfaf67a7d31f017ac3e2b3772245b5eb2
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-19 20:55:44 -0700

    Operator Overloading Assignment

---
+++ OperatorOverloadingAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OperatorOverloadingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee example = new Employee();
+
+            example.FirstName = "Suzanne";
+            example.LastName = "Timm";
+            example.ID = 123;
+
+            Employee example2 = new Employee();
+            example2.FirstName = "Hank";
+            example2.LastName = "van Tunen";
+            example2.ID = 456;
+            
+        }
+    }
+}
