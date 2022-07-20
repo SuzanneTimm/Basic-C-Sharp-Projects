79a4749afbd7e9a1009b95511ab691e013142295
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-20 08:31:45 -0700

    Added calls to test operator overloading

--- OperatorOverloadingAssignment/Program.cs
+++ OperatorOverloadingAssignment/Program.cs
@@ -1,22 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace OperatorOverloadingAssignment
+namespace LambdaExpressionAssignment
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Employee example = new Employee();
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee(1, "Deane", "Hubba"));
+            employees.Add(new Employee(2, "Leghorn", "Foghorn"));
+            employees.Add(new Employee(3, "Randy", "Dandy"));
+            employees.Add(new Employee(4, "Bello", "Cello"));
+            employees.Add(new Employee(5, "Joe", "Bing"));
+            employees.Add(new Employee(6, "Henry", "Cavill"));
+            employees.Add(new Employee(7, "Susie", "Cutie"));
+            employees.Add(new Employee(8, "Datz", "Daone"));
+            employees.Add(new Employee(9, "Joe", "Blow"));
+            employees.Add(new Employee(10, "Bob", "Apples"));
 
-            example.FirstName = "Suzanne";
-            example.LastName = "Timm";
-            example.ID = 123;
+            PrintEmployees("Original Employee List", employees);
 
-            Employee example2 = new Employee();
-            example2.FirstName = "Hank";
-            example2.LastName = "van Tunen";
-            example2.ID = 456;
-            
+            List<Employee> joes = new List<Employee>();
+            foreach (Employee employee in employees)
+            {               
+                if (employee.FirstName == "Joe")
+                {
+                    joes.Add(employee);
+                }
+            }
+            PrintEmployees("List of Joes using foreach.", joes);
+
+            List<Employee> joes2 = employees.FindAll(e => e.FirstName == "Joe");
+            PrintEmployees("List of Joes using Lambda.", joes2);
+
+            List<Employee> greaterThanFive = employees.FindAll(e => e.Id > 5 );
+            PrintEmployees("List of employees with id greater than 5.", greaterThanFive);
+        }
+
+        static void PrintEmployees ( string msg, List<Employee> list)
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine(new String('-', msg.Length));
+            list.ForEach(e => Console.WriteLine(e.FirstName + " " + e.LastName + ", Id = " + e.Id));
+            Console.WriteLine();
         }
     }
 }
