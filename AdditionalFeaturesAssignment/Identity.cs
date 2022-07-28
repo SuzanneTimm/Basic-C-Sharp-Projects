be1f1750a352ae763fbf8dcd01198d553dea20ca
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-28 15:24:17 -0700

    Additional Features Assignment

---
+++ AdditionalFeaturesAssignment/Identity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdditionalFeaturesAssignment
+
+{
+    public class Identity
+    {
+        public string name;
+        public int id;
+
+        public Identity(string name) : this(name, 0)
+        {
+
+        }
+
+        public Identity(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }     
+       
+    }
+}
---
+++ AdditionalFeaturesAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AdditionalFeaturesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+
+            const string myIdentity = "Suzanne";
+            Console.WriteLine("Hello, {0}.", myIdentity);
+
+            var identity = new Identity(myIdentity);           
+            Console.WriteLine(identity.id);
+            Console.WriteLine(identity.name);
+            Console.ReadLine();
+        }
+    }
+}
