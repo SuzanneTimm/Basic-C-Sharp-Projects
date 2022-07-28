1d72ccc88f92dd92ae23899f5c5ce320cdcb0cc7
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-27 17:27:07 -0700

    File I/O Assignment

---
+++ FileIOAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select a number please?");
+            string numberSelected = Console.ReadLine();
+            File.WriteAllText("Number.txt", numberSelected);
+            string fromFile = File.ReadAllText("Number.txt");
+            Console.WriteLine(fromFile);
+            Console.ReadLine();
+        }
+    }
+}
