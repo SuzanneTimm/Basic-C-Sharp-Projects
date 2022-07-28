ddb733a4d88e1f5b6336945fabf9311eb68d3da3
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-07-27 17:51:22 -0700

    Date Time Assignment

---
+++ DateTimeAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+            Console.WriteLine("Please, select a number?");
+            int numberSelected = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} + {1}hrs = {2} ", now, numberSelected, now.AddHours(numberSelected));
+
+            Console.ReadLine();
+        }
+    }
+}
