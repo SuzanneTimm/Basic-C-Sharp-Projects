ef654d5d648ef5159d412812747436b378d66427
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-08-06 15:36:57 -0700

    Final Challenge Initial Upload

---
+++ FinalChallenge/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalChallenge.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string CurrentDisplayDateAndTimeStr
+        {
+            get;
+            set;
+        }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+
+        {
+            CurrentDisplayDateAndTimeStr = DateTime.Now.ToString("D");
+
+        }
+        public void OnPost()
+
+        {
+            CurrentDisplayDateAndTimeStr = "Posted new time at " + DateTime.Now.ToString("F");
+        }
+    }
+}
