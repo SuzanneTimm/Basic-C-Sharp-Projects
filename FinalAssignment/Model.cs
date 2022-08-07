399da9b781704e7003abf9cfe99528723a71144f
Suzanne Timm <100321767+SuzanneTimm@users.noreply.github.com>
2022-08-06 17:03:41 -0700

    Final Assignment

---
+++ FinalAssignment/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Text;
+
+namespace FinalAssignment
+{
+   public class Student
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class StudentContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+    }
+
+
+
+}
---
+++ FinalAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace FinalAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the following information to create a student.");
+            Console.WriteLine();
+            Console.Write("First Name ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name ");
+            string lastName = Console.ReadLine();
+            Console.Write("Date of Birth ");
+            DateTime birthdate = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Email ");
+            string email = Console.ReadLine();
+
+            var student = new Student {
+                Id = Guid.NewGuid(),
+                FirstName = firstName, 
+                LastName = lastName,
+                BirthDate = birthdate,
+                Email = email };
+
+            using (var db = new StudentContext())
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                var query = from s in db.Students
+                            orderby s.LastName, s.FirstName
+                            select s;
+
+                Console.WriteLine();
+                Console.WriteLine("These are the students in the database.");
+                foreach (var std in query)
+                {
+                    Console.WriteLine(std.FirstName + " " + std.LastName);
+                }
+            }
+        }
+    }
+}
